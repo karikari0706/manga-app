29a4c40a262402d96638198a8779b464cf9515fa
karikari0706 <24aj013@ms.dendai.ac.jp>
2026-06-22 12:15:30 +0900

    Update add_data.py

--- add_data.py
+++ add_data.py
@@ -26,7 +26,7 @@ try:
         # 3. 本物の漫画データの登録 (タイトル, 作者, あらすじ, 画像URL, ジャンルIDのリスト)
         mangas = [
             ("呪術廻戦", "芥見下々", "呪いを祓う呪術師たちの闘いを描くダークファンタジー。", "https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088815169/1200/9784088815169.jpg", [1, 2]),
-            ("葬送のフリーレン", "山田鐘人・アベツカサ", "魔王討伐後のエルフの魔法使いの旅路。", "https://m.media-amazon.com/images/I/71YCYbEFgIL._AC_UF1000,1000_QL80_.jpg", [2, 4]),
+            ("葬送のフリーレン", "山田鐘人・アベツカサ", "魔王討伐後のエルフの魔法使いの旅路。", "https://assets.mercari-shops-static.com/-/large/plain/JPvn4CidkXC42D3Kz4cHnH.jpg@jpg", [2, 4]),
             ("かぐや様は告らせたい", "赤坂アカ", "天才たちの恋愛頭脳戦を描くラブコメディ。", "https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088904320/1200/9784088904320.jpg", [3, 4]),
             ("チェンソーマン", "藤本タツキ", "悪魔を狩るデビルのダークヒーローアクション。", "https://dosbg3xlm0x1t.cloudfront.net/images/items/08881780881780315501/1200/08881780881780315501.jpg", [1, 5]),
             ("SPY×FAMILY", "遠藤達哉", "スパイと超能力者と殺し屋の仮初めの家族を描くコメディ。", "https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088820118/1200/9784088820118.jpg", [1, 4])
